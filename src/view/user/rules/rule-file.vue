bfab031b68b093870c837fffce6e9ea0416848c5
Stefan-yb-bear <2223416037@qq.com>
2024-02-18 17:08:57 +0800

    协议规则

--- src/view/user/rules/rule-file.vue
+++ src/view/user/rules/rule-file.vue
@@ -1,57 +1,72 @@
 <template>
-  <File00 v-if="index === '0-0'" />
-  <File01 v-if="index === '0-1'" />
-  <File02 v-if="index === '0-2'" />
-  <File03 v-if="index === '0-3'" />
-  <File04 v-if="index === '0-4'" />
-  <File10 v-if="index === '1-0'" />
-  <File11 v-if="index === '1-1'" />
-  <File12 v-if="index === '1-2'" />
-  <File13 v-if="index === '1-3'" />
-  <File15 v-if="index === '1-4'" />
-  <File16 v-if="index === '1-5'" />
-  <File17 v-if="index === '1-6'" />
-  <File18 v-if="index === '1-7'" />
-  <File19 v-if="index === '1-8'" />
-  <File110 v-if="index === '1-9'" />
-  <File111 v-if="index === '1-10'" />
-  <File112 v-if="index === '1-11'" />
-  <File113 v-if="index === '1-12'" />
-  <File20 v-if="index === '2-0'" />
-  <File21 v-if="index === '2-1'" />
-  <File22 v-if="index === '2-2'" />
-  <File23 v-if="index === '2-3'" />
-  <File24 v-if="index === '2-4'" />
-  <File25 v-if="index === '2-5'" />
-  <File26 v-if="index === '2-6'" />
-  <File27 v-if="index === '2-7'" />
-  <File28 v-if="index === '2-8'" />
-  <File29 v-if="index === '2-9'" />
-  <File210 v-if="index === '2-10'" />
-  <File211 v-if="index === '2-11'" />
-  <File212 v-if="index === '2-12'" />
-  <File213 v-if="index === '2-13'" />
-  <File214 v-if="index === '2-14'" />
-  <File215 v-if="index === '2-15'" />
-  <File30 v-if="index === '3-0'" />
-  <File31 v-if="index === '3-1'" />
-  <File32 v-if="index === '3-2'" />
-  <File33 v-if="index === '3-3'" />
-  <File34 v-if="index === '3-4'" />
-  <File35 v-if="index === '3-5'" />
-  <File36 v-if="index === '3-6'" />
-  <File37 v-if="index === '3-7'" />
-  <File38 v-if="index === '3-8'" />
-  <File39 v-if="index === '3-9'" />
-  <File310 v-if="index === '3-10'" />
-  <File311 v-if="index === '3-11'" />
-  <File312 v-if="index === '3-12'" />
-  <File313 v-if="index === '3-13'" />
+  <div class="chapterArea">
+    <File00 v-if="index === '0-0'" />
+    <File01 v-if="index === '0-1'" />
+    <File02 v-if="index === '0-2'" />
+    <File03 v-if="index === '0-3'" />
+    <File04 v-if="index === '0-4'" />
+    <File10 v-if="index === '1-0'" />
+    <File11 v-if="index === '1-1'" />
+    <File12 v-if="index === '1-2'" />
+    <File13 v-if="index === '1-3'" />
+    <File15 v-if="index === '1-4'" />
+    <File16 v-if="index === '1-5'" />
+    <File17 v-if="index === '1-6'" />
+    <File18 v-if="index === '1-7'" />
+    <File19 v-if="index === '1-8'" />
+    <File110 v-if="index === '1-9'" />
+    <File111 v-if="index === '1-10'" />
+    <File112 v-if="index === '1-11'" />
+    <File113 v-if="index === '1-12'" />
+    <File20 v-if="index === '2-0'" />
+    <File21 v-if="index === '2-1'" />
+    <File22 v-if="index === '2-2'" />
+    <File23 v-if="index === '2-3'" />
+    <File24 v-if="index === '2-4'" />
+    <File25 v-if="index === '2-5'" />
+    <File26 v-if="index === '2-6'" />
+    <File27 v-if="index === '2-7'" />
+    <File28 v-if="index === '2-8'" />
+    <File29 v-if="index === '2-9'" />
+    <File210 v-if="index === '2-10'" />
+    <File211 v-if="index === '2-11'" />
+    <File212 v-if="index === '2-12'" />
+    <File213 v-if="index === '2-13'" />
+    <File214 v-if="index === '2-14'" />
+    <File215 v-if="index === '2-15'" />
+    <File30 v-if="index === '3-0'" />
+    <File31 v-if="index === '3-1'" />
+    <File32 v-if="index === '3-2'" />
+    <File33 v-if="index === '3-3'" />
+    <File34 v-if="index === '3-4'" />
+    <File35 v-if="index === '3-5'" />
+    <File36 v-if="index === '3-6'" />
+    <File37 v-if="index === '3-7'" />
+    <File38 v-if="index === '3-8'" />
+    <File39 v-if="index === '3-9'" />
+    <File310 v-if="index === '3-10'" />
+    <File311 v-if="index === '3-11'" />
+    <File312 v-if="index === '3-12'" />
+    <File313 v-if="index === '3-13'" />
+  </div>
+  <nav class="product-nav">
+    <ul>
+      <li v-for="(nav, index) in navs" :class="{
+        nav: true,
+        'nav-active': index == activeNav - 1
+      }">
+        <span></span>
+        <span @click="goToPosition(index + 1)">
+          {{ nav }}
+        </span>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script setup>
-import { onMounted, onUpdated } from 'vue'
-import { marked } from 'marked'
+import { onMounted, onUpdated, ref } from 'vue'
+// import { marked } from 'marked'
 import File00 from './components/File00.vue'
 import File01 from './components/File01.vue'
 import File02 from './components/File02.vue'
@@ -107,10 +122,10 @@ defineProps({
   }
 })
 const update = () => {
-  const file = document.querySelector('pre')
-  if (file) {
-    file.innerHTML = marked.parse(file.textContent)
-  }
+  // const file = document.querySelector('pre')
+  // if (file) {
+  //   file.innerHTML = marked.parse(file.textContent)
+  // }
 }
 onMounted(() => {
   update()
@@ -118,19 +133,86 @@ onMounted(() => {
 onUpdated(() => {
   update()
 })
+
+const navs = ['01 提示条款', '02 平台声明', '03 账户风险提示', '04 信息风险提示', '05 平台风险提示', '06 其他风险提示']
+//导航逻辑
+const goToPosition = id => {
+  setActive(id)
+  const element = document.getElementById(id)
+  element.scrollIntoView()
+}
+const activeNav = ref(0)
+const setActive = id => {
+  activeNav.value = id
+}
 </script>
 <style lang="scss">
+.chapterArea {
+  width: 90%;
+
+}
+
+.text {
+  color: #000000;
+  font-size: 14px;
+  font-family: PingFang SC;
+  letter-spacing: 1px;
+  line-height: 1.4;
+  text-align: left;
+  padding: 0;
+  width: 75%;
+}
+
+.title {
+  line-height: 1;
+  text-align: left;
+  color: #000000;
+  font-family: PingFang SC;
+  font-size: 32px;
+  text-shadow: 2px 3px 3px rgba(0, 0, 0, 0.2);
+}
+
+.date {
+  line-height: 1;
+  color: rgb(117, 117, 117);
+  padding-bottom: 5px;
+  font-size: 15px;
+  font-family: PingFang SC;
+  border-bottom: 3px solid rgb(216, 216, 216);
+}
+
+.greenTitle {
+  line-height: 1;
+  height: 40px;
+  line-height: 40px;
+  font-size: 26px;
+  font-family: PingFang SC;
+  color: #95D600;
+  padding-left: 10px;
+  border-left: 6px solid #95D600;
+  font-weight: 400;
+}
+
+.hightLight {
+  color: #95D600;
+  background-color: rgb(254, 249, 161);
+}
+
 el-main {
   width: 200px;
   display: flex;
   flex-wrap: wrap;
 }
+
+//1.商品发布是否公开
+//2.规则协议
 pre {
   white-space: pre-wrap;
   padding: 40px;
   word-break: break-all;
   word-wrap: break-word;
   font-family: Arial, Helvetica, sans-serif;
+
   img {
     border-radius: 10px;
     width: 70vw;
@@ -138,27 +220,33 @@ pre {
     margin: 0 auto;
 
   }
+
   table {
     border-spacing: 0;
     width: 100%;
   }
+
   td,
   th {
     border: 0.2px solid #666;
     padding: 5px;
   }
 }
+
 @media (max-width: 768px) {
   aside {
     display: none;
   }
+
   .el-header {
     display: none;
   }
+
   pre {
     white-space: initial;
   }
 }
+
 .carousel {
   position: relative;
   width: 100%;
@@ -185,26 +273,72 @@ pre {
   height: 100%;
   object-fit: cover;
 }
-pre{
+
+pre {
   font-size: 1.8vh;
-  li{
+
+  li {
     margin-left: 3vw;
   }
-  p{
+
+  p {
     line-height: 3.5vh;
-    strong{
+
+    strong {
       position: relative;
       line-height: 10px;
     }
   }
-  a{
+
+  a {
     display: inline-block;
     width: auto;
   }
-  h1{
+
+  h1 {
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+}
+
+.product-nav {
+  position: fixed;
+  // left: 1300px;
+  // right: 50px;
+  left: calc(100% - 160px);
+  top: 130px;
+  border-left: 2px solid lightgray;
+
+  li {
+    list-style: none;
+    min-width: 70px;
+    margin: 14px;
+    cursor: pointer;
+  }
+
+  .nav-active {
+    span {
+      position: relative;
+      height: 30px;
+      color: var(--el-color-primary);
+    }
+
+    span:before {
+      content: '';
+      position: absolute;
+      left: -16px;
+      width: 2px;
+      height: 100%;
+      background-color: var(--el-color-primary);
+      transform: scale(1.5, 2);
+      z-index: 10;
+    }
+  }
+
+  span {
+    position: relative;
+    color: #8a8a8a;
   }
 }
 </style>
