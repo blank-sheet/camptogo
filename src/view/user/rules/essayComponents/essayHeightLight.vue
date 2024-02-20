568451765736a5d10462e786c650bb4548b26d44
Stefan-yb-bear <2223416037@qq.com>
2024-02-20 15:42:17 +0800

    链接

--- src/view/user/rules/essayComponents/essayHeightLight.vue
+++ src/view/user/rules/essayComponents/essayHeightLight.vue
@@ -8,7 +8,7 @@
 
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .hightLight {
   color: #95D600;
   background-color: rgb(254, 249, 161);
@@ -16,4 +16,10 @@
   padding: 0;
 }
 
+a {
+  color: #95D600;
+  background-color: rgb(254, 249, 161);
+  margin: 0;
+  padding: 0;
+}
 </style>
