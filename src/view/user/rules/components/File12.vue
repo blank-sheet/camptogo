83f85092ddd1f78370042363814d5c08aa368888
Stefan-yb-bear <2223416037@qq.com>
2024-02-28 17:20:10 +0800

    规则协议文本空格file1~13

--- src/view/user/rules/components/File12.vue
+++ src/view/user/rules/components/File12.vue
@@ -6,133 +6,133 @@
 生效日期：2024年1月21日</essayDate>
 <essayTitleH2>第一章 什么是优惠券</essayTitleH2>
 
-<essayTextWithWeight>1.1</essayTextWithWeight>营探（www.camptogo.com）优惠券（以下简称优惠券）是营探联合服务商对广大消费者的一种优惠和回馈。为使更多的消费者享受到这种优惠，特制定《营探优惠券使用规则》（以下简称本规则）。如果您接受并使用营探优惠券，那么您便接受了本规则。
+<essayTextWithWeight>1.1</essayTextWithWeight> 营探（www.camptogo.com）优惠券（以下简称优惠券）是营探联合服务商对广大消费者的一种优惠和回馈。为使更多的消费者享受到这种优惠，特制定《营探优惠券使用规则》（以下简称本规则）。如果您接受并使用营探优惠券，那么您便接受了本规则。
 
-<essayTextWithWeight>1.2</essayTextWithWeight>优惠券仅在营探消费者为个人消费而发生购买行为时使用，不适用于团体购买、企业顾客购买及其他非以个人消费为目的的购买行为，使用他人手机号、地址等信息注册，或以非正常手段注册、一人注册多个账户等视为违规注册，同一手机、同一IP地址、同一或高度相似收货地址、同一设备号、同一ID、同一收件人或其他可以合理显示为同一账户的情形，其中任意一项或数项符合均视为同一账户。
+<essayTextWithWeight>1.2</essayTextWithWeight> 优惠券仅在营探消费者为个人消费而发生购买行为时使用，不适用于团体购买、企业顾客购买及其他非以个人消费为目的的购买行为，使用他人手机号、地址等信息注册，或以非正常手段注册、一人注册多个账户等视为违规注册，同一手机、同一IP地址、同一或高度相似收货地址、同一设备号、同一ID、同一收件人或其他可以合理显示为同一账户的情形，其中任意一项或数项符合均视为同一账户。
 
-<essayTextWithWeight>1.3</essayTextWithWeight>优惠券的使用受订单金额和商品和服务的限制，订单必须符合优惠券的使用条件才可使用，优惠券分为以下几类：
+<essayTextWithWeight>1.3</essayTextWithWeight> 优惠券的使用受订单金额和商品和服务的限制，订单必须符合优惠券的使用条件才可使用，优惠券分为以下几类：
 
-1.3.1满减券：用来直接减免部分订单金额的优惠券；
+<essayTextWithWeight>1.3.1</essayTextWithWeight> 满减券：用来直接减免部分订单金额的优惠券；
 
-1.3.2打折券：通过折扣的形式进行订单金额减免的优惠券；
+<essayTextWithWeight>1.3.2</essayTextWithWeight> 打折券：通过折扣的形式进行订单金额减免的优惠券；
 
-1.3.3店铺券：仅在某一服务商提供的商品和服务中可使用的优惠券；
+<essayTextWithWeight>1.3.3</essayTextWithWeight> 店铺券：仅在某一服务商提供的商品和服务中可使用的优惠券；
 
-1.3.4平台通用券：凡是参与营探全场平台抵扣的商品和服务均可以使用的优惠券；
+<essayTextWithWeight>1.3.4</essayTextWithWeight> 平台通用券：凡是参与营探全场平台抵扣的商品和服务均可以使用的优惠券；
 
-1.3.5品类券：只针对某一类别商品和服务使用的优惠券；
+<essayTextWithWeight>1.3.5</essayTextWithWeight> 品类券：只针对某一类别商品和服务使用的优惠券；
 
-1.3.6单品券：只针对某一个指定商品和服务使用的优惠券；
+<essayTextWithWeight>1.3.6</essayTextWithWeight> 单品券：只针对某一个指定商品和服务使用的优惠券；
 
-1.3.7平台会员优惠券：是成为营探会员身份可领取使用的优惠券。  
+<essayTextWithWeight>1.3.7</essayTextWithWeight> 平台会员优惠券：是成为营探会员身份可领取使用的优惠券。  
 
 
 
 <essayTitleH2>第二章 如何获得优惠券</essayTitleH2>
 
-<essayTextWithWeight>2.1</essayTextWithWeight>优惠券采用系统自动发放和消费者主动领取两种形式。
+<essayTextWithWeight>2.1</essayTextWithWeight> 优惠券采用系统自动发放和消费者主动领取两种形式。
 
-<essayTextWithWeight>2.2</essayTextWithWeight>消费者主动领取的优惠券可以通过商品和服务详情页、活动促销页面、礼券中心等页面进行领取。
+<essayTextWithWeight>2.2</essayTextWithWeight> 消费者主动领取的优惠券可以通过商品和服务详情页、活动促销页面、礼券中心等页面进行领取。
 
-<essayTextWithWeight>2.3</essayTextWithWeight>消费者可能通过邀请、注册、抽奖、积分兑换、满赠、线上活动、推广活动、购买赠与等形式获得优惠券，参加活动并满足活动促销要求可获得优惠券，活动信息请持续关注营探各活动信息及广告内容。
+<essayTextWithWeight>2.3</essayTextWithWeight> 消费者可能通过邀请、注册、抽奖、积分兑换、满赠、线上活动、推广活动、购买赠与等形式获得优惠券，参加活动并满足活动促销要求可获得优惠券，活动信息请持续关注营探各活动信息及广告内容。
 
-<essayTextWithWeight>2.4</essayTextWithWeight>服务商发放优惠券的，由服务商100%承担费用；平台发放优惠券的，费用根据当时的联合促销政策按约定比例承担。
+<essayTextWithWeight>2.4</essayTextWithWeight> 服务商发放优惠券的，由服务商100%承担费用；平台发放优惠券的，费用根据当时的联合促销政策按约定比例承担。
 
-<essayTextWithWeight>2.5</essayTextWithWeight>优惠券的设置、发放与适用范围，服务商可通过工作台进行设置，商品发布生效后即按照相关设置履行。
+<essayTextWithWeight>2.5</essayTextWithWeight> 优惠券的设置、发放与适用范围，服务商可通过工作台进行设置，商品发布生效后即按照相关设置履行。
 
 
 
 <essayTitleH2>第三章 如何查看优惠券</essayTitleH2>
 
-<essayTextWithWeight>3.1</essayTextWithWeight>进入“我的－礼券中心”，在此可查看您拥有的所有待使用、已使用、已过期的优惠券。
+<essayTextWithWeight>3.1</essayTextWithWeight> 进入“我的－礼券中心”，在此可查看您拥有的所有待使用、已使用、已过期的优惠券。
 
-<essayTextWithWeight>3.2</essayTextWithWeight>进入“立即预订-填写订单-订单明细”，可点击查看已有优惠券，并在支付前进行勾选。
+<essayTextWithWeight>3.2</essayTextWithWeight> 进入“立即预订-填写订单-订单明细”，可点击查看已有优惠券，并在支付前进行勾选。
 
 
 
 
 <essayTitleH2>第四章 如何使用优惠券</essayTitleH2>
 
-<essayTextWithWeight>4.1</essayTextWithWeight>您须完成营探账户的注册和登录。
+<essayTextWithWeight>4.1</essayTextWithWeight> 您须完成营探账户的注册和登录。
 
-<essayTextWithWeight>4.2</essayTextWithWeight>登录营探账户后，您须在提交订单前完成实名认证。
+<essayTextWithWeight>4.2</essayTextWithWeight> 登录营探账户后，您须在提交订单前完成实名认证。
 
-<essayTextWithWeight>4.3</essayTextWithWeight>使用步骤
+<essayTextWithWeight>4.3</essayTextWithWeight> 使用步骤
 
-4.3.1进入商品页面，点击立即预订；
+<essayTextWithWeight>4.3.1</essayTextWithWeight> 进入商品页面，点击立即预订；
 
-4.3.2进入填写订单页面，完善相关信息；
+<essayTextWithWeight>4.3.2</essayTextWithWeight> 进入填写订单页面，完善相关信息；
 
-4.3.3提交订单前，在填写订单页面-订单明细-优惠券栏，点击“立即使用”；
+<essayTextWithWeight>4.3.3</essayTextWithWeight> 提交订单前，在填写订单页面-订单明细-优惠券栏，点击“立即使用”；
 
-4.3.4点击后，系统自动跳转至礼券中心页面，展示出可使用的优惠券，您可选择并点击一张优惠券，之后系统会自动跳转回填写订单页面，并计算实际付款金额，经您确认后，点击提交订单即可。
+<essayTextWithWeight>4.3.4</essayTextWithWeight> 点击后，系统自动跳转至礼券中心页面，展示出可使用的优惠券，您可选择并点击一张优惠券，之后系统会自动跳转回填写订单页面，并计算实际付款金额，经您确认后，点击提交订单即可。
 
 
 
 
 <essayTitleH2>第五章 优惠券使用细则</essayTitleH2>
 
-<essayTextWithWeight>5.1</essayTextWithWeight>优惠券仅限于使用营探APP/小程序展示的对象产品，并且仅在APP/小程序订购时使用。
+<essayTextWithWeight>5.1</essayTextWithWeight> 优惠券仅限于使用营探APP/小程序展示的对象产品，并且仅在APP/小程序订购时使用。
 
-<essayTextWithWeight>5.2</essayTextWithWeight>优惠券仅抵扣商品价格，不适用服务费、税费、运费及其他费用。
+<essayTextWithWeight>5.2</essayTextWithWeight> 优惠券仅抵扣商品价格，不适用服务费、税费、运费及其他费用。
 
-<essayTextWithWeight>5.3</essayTextWithWeight>优惠券不能兑换现金。
+<essayTextWithWeight>5.3</essayTextWithWeight> 优惠券不能兑换现金。
 
-<essayTextWithWeight>5.4</essayTextWithWeight>每张优惠券仅能使用一次，不记名、不挂失、不找零、不退换。
+<essayTextWithWeight>5.4</essayTextWithWeight> 每张优惠券仅能使用一次，不记名、不挂失、不找零、不退换。
 
-<essayTextWithWeight>5.5</essayTextWithWeight>使用优惠券抵扣部分的金额不开具发票。
+<essayTextWithWeight>5.5</essayTextWithWeight> 使用优惠券抵扣部分的金额不开具发票。
 
-<essayTextWithWeight>5.6</essayTextWithWeight>优惠券应在券面标示或系统显示的有效期内使用，过期作废，不能延期使用。
+<essayTextWithWeight>5.6</essayTextWithWeight> 优惠券应在券面标示或系统显示的有效期内使用，过期作废，不能延期使用。
 
-<essayTextWithWeight>5.7</essayTextWithWeight>消费者同意按照具体优惠券规则使用优惠券。
+<essayTextWithWeight>5.7</essayTextWithWeight> 消费者同意按照具体优惠券规则使用优惠券。
 
-<essayTextWithWeight>5.8</essayTextWithWeight>一般情况下，一张订单仅限使用一张优惠券，不可拆分；特殊情况，多个优惠券可叠加使用的，详情见优惠券具体说明。
+<essayTextWithWeight>5.8</essayTextWithWeight> 一般情况下，一张订单仅限使用一张优惠券，不可拆分；特殊情况，多个优惠券可叠加使用的，详情见优惠券具体说明。
 
-<essayTextWithWeight>5.9</essayTextWithWeight>在符合使用优惠券条件的商品和服务上，优惠券会按数额抵扣在订单金额中；特例商品和服务不能使用优惠券预订，具体以商品和服务披露为准。
+<essayTextWithWeight>5.9</essayTextWithWeight> 在符合使用优惠券条件的商品和服务上，优惠券会按数额抵扣在订单金额中；特例商品和服务不能使用优惠券预订，具体以商品和服务披露为准。
 
-<essayTextWithWeight>5.10</essayTextWithWeight>使用优惠券的订单，如消费者在订单生成后30分钟内仍未支付成功，系统将自动取消订单。优惠券将退回账号礼券中心，有效期不变。
+<essayTextWithWeight>5.10</essayTextWithWeight> 使用优惠券的订单，如消费者在订单生成后30分钟内仍未支付成功，系统将自动取消订单。优惠券将退回账号礼券中心，有效期不变。
 
-<essayTextWithWeight>5.11</essayTextWithWeight>使用优惠券并已支付成功的订单，若消费者在活动开营前申请退款，已使用的优惠券将作废，不能再次使用，也不再补发；若服务商在活动开营前取消活动，优惠券将退回账号礼券中心，有效期不变；活动开营后，优惠券一律不予退还。
+<essayTextWithWeight>5.11</essayTextWithWeight> 使用优惠券并已支付成功的订单，若消费者在活动开营前申请退款，已使用的优惠券将作废，不能再次使用，也不再补发；若服务商在活动开营前取消活动，优惠券将退回账号礼券中心，有效期不变；活动开营后，优惠券一律不予退还。
 
-<essayTextWithWeight>5.12</essayTextWithWeight>由于账户被盗用等非常规原因或消费者自身原因造成不能使用优惠券的情况，与营探无关。
+<essayTextWithWeight>5.12</essayTextWithWeight> 由于账户被盗用等非常规原因或消费者自身原因造成不能使用优惠券的情况，与营探无关。
 
-<essayTextWithWeight>5.13</essayTextWithWeight>使用优惠券并已支付成功的订单，若消费者在活动开营前申请退款：购买单个商品和服务的优惠，按购买单品时实际支付金额退款；购买多个商品和服务的优惠，按被取消的商品和服务售价所占订单金额比例拆分实际支付金额进行退款，未取消的商品和服务仍按支付时的优惠比例享受优惠。已使用优惠券不退还。
+<essayTextWithWeight>5.13</essayTextWithWeight> 使用优惠券并已支付成功的订单，若消费者在活动开营前申请退款：购买单个商品和服务的优惠，按购买单品时实际支付金额退款；购买多个商品和服务的优惠，按被取消的商品和服务售价所占订单金额比例拆分实际支付金额进行退款，未取消的商品和服务仍按支付时的优惠比例享受优惠。已使用优惠券不退还。
 
 例如：您购买了价格为80元的商品A，和2件价格为60元的商品B，使用了全场满200减50元的优惠券一张，实际付款为150元。
 
-5.13.1 当您单独申请A商品退款时，应退金额为：150*（80/200）=150*0.4=60元
+<essayTextWithWeight>5.13.1</essayTextWithWeight> 当您单独申请A商品退款时，应退金额为：150*（80/200）=150*0.4=60元
 
-5.13.2 当您单独申请其中1件B商品退款时，应退金额为：150*（60/200）=150*0.3=45元
+<essayTextWithWeight>5.13.2</essayTextWithWeight> 当您单独申请其中1件B商品退款时，应退金额为：150*（60/200）=150*0.3=45元
 
-<essayTextWithWeight>5.14</essayTextWithWeight>禁止同一消费者非以个人消费为目的大量购买产品套取平台优惠。营探有权根据自身掌握的情况独立判断参与活动的消费者是否实施了违规行为，如消费者违反本规则，营探有收回已领用的优惠券及对消费者进行进一步处理甚至冻结其账号的权利。如果消费者所使用的电话或其他关联信息是营探平台所认定的 "黄牛" 类型，营探平台有权取消其参与资格及收回已获得的相关优惠券等。
+<essayTextWithWeight>5.14</essayTextWithWeight> 禁止同一消费者非以个人消费为目的大量购买产品套取平台优惠。营探有权根据自身掌握的情况独立判断参与活动的消费者是否实施了违规行为，如消费者违反本规则，营探有收回已领用的优惠券及对消费者进行进一步处理甚至冻结其账号的权利。如果消费者所使用的电话或其他关联信息是营探平台所认定的 "黄牛" 类型，营探平台有权取消其参与资格及收回已获得的相关优惠券等。
 
-<essayTextWithWeight>5.15</essayTextWithWeight>违背优惠券使用规则的处理发生以下情形时，营探有权视情况选择冻结或作废相关消费者持有的优惠券、取消使用优惠券的订单、取消该消费者的用券资格、停止提供服务或注销该消费者账户并有权不接受该消费者的再次注册申请：
+<essayTextWithWeight>5.15</essayTextWithWeight> 违背优惠券使用规则的处理发生以下情形时，营探有权视情况选择冻结或作废相关消费者持有的优惠券、取消使用优惠券的订单、取消该消费者的用券资格、停止提供服务或注销该消费者账户并有权不接受该消费者的再次注册申请：
 
-5.15.1 非以个人消费为目的而使用优惠券的；
+<essayTextWithWeight>5.15.1</essayTextWithWeight> 非以个人消费为目的而使用优惠券的；
 
-5.15.2 伪造、购买、销售、转让优惠券，或使用伪造、购买、转让的优惠券的；
+<essayTextWithWeight>5.15.2</essayTextWithWeight> 伪造、购买、销售、转让优惠券，或使用伪造、购买、转让的优惠券的；
 
-5.15.3 违反本规则或优惠券中载明的规则，使用优惠券的。
+<essayTextWithWeight>5.15.3</essayTextWithWeight> 违反本规则或优惠券中载明的规则，使用优惠券的。
 
-<essayTextWithWeight>5.16</essayTextWithWeight>营探标明不参加优惠活动的商品和服务，订购时无法使用优惠券，敬请谅解。
+<essayTextWithWeight>5.16</essayTextWithWeight> 营探标明不参加优惠活动的商品和服务，订购时无法使用优惠券，敬请谅解。
 
-<essayTextWithWeight>5.17</essayTextWithWeight>优惠券的使用金额和限制、使用时间等使用条件以实际订单或活动说明为准。
+<essayTextWithWeight>5.17</essayTextWithWeight> 优惠券的使用金额和限制、使用时间等使用条件以实际订单或活动说明为准。
 
-<essayTextWithWeight>5.18</essayTextWithWeight>优惠券与本规则中载明的规则不一致时，以优惠券为准；优惠券中未载明的事项，如本规则有规定的，对优惠券的使用亦发生效力。
+<essayTextWithWeight>5.18</essayTextWithWeight> 优惠券与本规则中载明的规则不一致时，以优惠券为准；优惠券中未载明的事项，如本规则有规定的，对优惠券的使用亦发生效力。
 
-<essayTextWithWeight>5.19</essayTextWithWeight>商品和服务活动价的折扣计算标准以活动前7日最低售价（以下简称原价）为基础，活动及单品表现出来的折扣信息不得高于（折后售价/原价）换算出来的折扣。原价是指经营者在本次降价前7日内在本经营场所成交的有交易票据的最低交易价格；如前7日内没有交易价格，则以本次降价前最后一次交易价格作为原价。
+<essayTextWithWeight>5.19</essayTextWithWeight> 商品和服务活动价的折扣计算标准以活动前7日最低售价（以下简称原价）为基础，活动及单品表现出来的折扣信息不得高于（折后售价/原价）换算出来的折扣。原价是指经营者在本次降价前7日内在本经营场所成交的有交易票据的最低交易价格；如前7日内没有交易价格，则以本次降价前最后一次交易价格作为原价。
 
-<essayTextWithWeight>5.20</essayTextWithWeight>明显价格标错
+<essayTextWithWeight>5.20</essayTextWithWeight> 明显价格标错
 
-5.20.1 明显价格标错，指因系统故障或人为原因导致未划线价格错标、优惠券混乱、折扣异常，且订单价格与市场价格相差巨大，即使是普通消费者也能够判断出该订单价格异常的情形。该情形下的低价交易使一方明显受损，有违交易主体公平自愿的基本民事原则，构成重大误解，消费者和服务商双方均有权申请撤销合同，营探也有权对相应的优惠券进行无效处理。
+<essayTextWithWeight>5.20.1</essayTextWithWeight>  明显价格标错，指因系统故障或人为原因导致未划线价格错标、优惠券混乱、折扣异常，且订单价格与市场价格相差巨大，即使是普通消费者也能够判断出该订单价格异常的情形。该情形下的低价交易使一方明显受损，有违交易主体公平自愿的基本民事原则，构成重大误解，消费者和服务商双方均有权申请撤销合同，营探也有权对相应的优惠券进行无效处理。
 
-5.20.2 明显价格错标可能由平台技术故障、商户操作失误或外部攻击等情形导致。
+<essayTextWithWeight>5.20.2</essayTextWithWeight>  明显价格错标可能由平台技术故障、商户操作失误或外部攻击等情形导致。
 
-5.20.3 因平台技术故障导致的明显价格标错，营探将在系统恢复后第一时间发布声明，公告平台技术故障及恢复情况，立即整改明显价格标错问题，同时对相应优惠券进行无效处理。
+<essayTextWithWeight>5.20.3</essayTextWithWeight>  因平台技术故障导致的明显价格标错，营探将在系统恢复后第一时间发布声明，公告平台技术故障及恢复情况，立即整改明显价格标错问题，同时对相应优惠券进行无效处理。
 
-5.20.4 因商户操作失误导致的明显价格标错，商户须在知道或应当知道之日起三（3）日内联系营探客服提交相对应的说明材料，否则发生违约将按一般情形处理，由服务商承担相对应的违约赔偿责任。
+<essayTextWithWeight>5.20.4</essayTextWithWeight>  因商户操作失误导致的明显价格标错，商户须在知道或应当知道之日起三（3）日内联系营探客服提交相对应的说明材料，否则发生违约将按一般情形处理，由服务商承担相对应的违约赔偿责任。
 
-5.20.5 因外部攻击导致的明显价格标错，如外部攻击对象为营探平台，按照平台技术故障情形进行处理，如外部攻击对象为服务商，商户须在知道或应当知道之日起<essayTextWithWeight>三（3）日</essayTextWithWeight>内联系营探客服提交相对应的说明材料，否则发生违约将按一般情形处理，服务商承担相对应的违约赔偿责任。
+<essayTextWithWeight>5.20.5</essayTextWithWeight>  因外部攻击导致的明显价格标错，如外部攻击对象为营探平台，按照平台技术故障情形进行处理，如外部攻击对象为服务商，商户须在知道或应当知道之日起<essayTextWithWeight>三（3）日</essayTextWithWeight>内联系营探客服提交相对应的说明材料，否则发生违约将按一般情形处理，服务商承担相对应的违约赔偿责任。
 
 
 
--- src/view/user/rules/essayComponents/essayTextWithWeight.vue
+++ src/view/user/rules/essayComponents/essayTextWithWeight.vue
@@ -1,15 +1,21 @@
 <template>
-  <span class="text">
+  <span class="text" :style="{ 'text-decoration': line ? 'underline' : '' }">
     <slot></slot>
   </span>
 </template>
 
 <script setup>
-
+const props = defineProps({
+  line: {
+    type: Boolean,
+    default: false
+  }
+})
 </script>
 
 <style lang="scss" scoped>
 .text {
   font-weight: 550;
+  color: #262626;
 }
 </style>
