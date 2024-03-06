f3de14675ab441e240249f7991bd05826e0aba86
Stefan-yb-bear <2223416037@qq.com>
2024-03-06 15:31:58 +0800

    日期线条

--- src/view/user/rules/essayComponents/essayDate.vue
+++ src/view/user/rules/essayComponents/essayDate.vue
@@ -13,7 +13,7 @@
   color: #8C8C8C;
   padding-bottom: 5px;
   font-family: PingFang SC;
-  border-bottom: 3px solid rgb(216, 216, 216);
+  border-bottom: 1px solid rgb(216, 216, 216);
   font-style: normal;
   font-weight: 400;
   font-size: 12px;
