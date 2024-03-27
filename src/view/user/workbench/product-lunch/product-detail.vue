63ac36acab9225eb3ca91453fc616766c8962443
Stefan-yb-bear <2223416037@qq.com>
2024-03-27 20:23:29 +0800

    视频无法删除

--- src/view/user/workbench/product-lunch/product-detail.vue
+++ src/view/user/workbench/product-lunch/product-detail.vue
@@ -411,15 +411,15 @@
               <div class="hardware">
                 <header>活动场地</header>
                 <el-input v-model="form.venue" autosize type="textarea" placeholder="您可以从以下方面描述室内室外活动场地：
-                        场地名称；场地类型，如运动场、实验室、专业草场等特色或专业场地；设施设备，如监控、新风系统、活动设施、专业设备等。" />
+                              场地名称；场地类型，如运动场、实验室、专业草场等特色或专业场地；设施设备，如监控、新风系统、活动设施、专业设备等。" />
               </div>
               <div class="hardware" style="margin-top: 20px" v-show="form.stayIf">
                 <header>住宿条件</header>
                 <el-input type="textarea" autosize v-model="form.accommodations" placeholder="您可以从以下方面描述住宿条件：
-                        住宿场所类型，如酒店、民宿、营房、青旅等；
-                        房间类型，如单人房、双人间、X人宿舍等；
-                        房间条件，如床型、热水、卫浴、空调、网络、电器等；
-                        安保情况，包括安保人员、夜间值班、巡逻情况等。" />
+                              住宿场所类型，如酒店、民宿、营房、青旅等；
+                              房间类型，如单人房、双人间、X人宿舍等；
+                              房间条件，如床型、热水、卫浴、空调、网络、电器等；
+                              安保情况，包括安保人员、夜间值班、巡逻情况等。" />
               </div>
               <div class="hardware">
                 <header>教学教具</header>
@@ -436,8 +436,8 @@
               <div class="hardware">
                 <header>团队详情</header>
                 <el-input style="width:80%" v-model="form.team.detail" type="textarea" autosize placeholder="您可以从以下方面进行描述：
-                        团队配置，如课程老师、助教老师、生活老师、安全员、医护人员、摄影老师、领队、导游等；
-                        人员详情，如资质、荣誉、教龄等。" />
+                              团队配置，如课程老师、助教老师、生活老师、安全员、医护人员、摄影老师、领队、导游等；
+                              人员详情，如资质、荣誉、教龄等。" />
               </div>
               <div class="hardware">
                 <header>专业拍摄</header>
@@ -453,8 +453,8 @@
                       v-model="form.team.captureNums" />个拍摄人员
                   </div>
                   <el-input style="width:80%" type="textarea" autosize placeholder="请从以下方面描述拍摄服务详情：
-                        预计产出照片张数；预计产出视频个数，单个视频长度；
-                        使用设备型号；机位个数及位置；其他。" v-model="form.team.captureDetail" />
+                              预计产出照片张数；预计产出视频个数，单个视频长度；
+                              使用设备型号；机位个数及位置；其他。" v-model="form.team.captureDetail" />
                 </div>
               </div>
             </CampFormItem>
@@ -474,10 +474,10 @@
             </CampFormItem>
             <CampFormItem label="医疗情况：" prop="medicalCare">
               <el-input style="width:80%" v-model="form.medicalCare" type="textarea" autosize placeholder="您可以从以下方面描述医疗情况：
-                        医疗人员情况，如专业医疗人员、员工医疗培训情况等；
-                        医疗场所情况，如是否有卫生室等；
-                        医疗物资状况，如急救箱、药品、医疗设备等；
-                        附近医疗点情况，如附近医院的距离、等级等。" />
+                              医疗人员情况，如专业医疗人员、员工医疗培训情况等；
+                              医疗场所情况，如是否有卫生室等；
+                              医疗物资状况，如急救箱、药品、医疗设备等；
+                              附近医疗点情况，如附近医院的距离、等级等。" />
             </CampFormItem>
             <CampFormItem label="图片视频上传" :msg="'图片和视频为必选项'">
               <span class="desc">
@@ -490,15 +490,23 @@
                     *请上传一段不超过2分钟，大小10M以内的短视频，内容为行前说明或活动风采展示
                   </span>
                 </div>
-                <el-upload name="image" :headers="authHeader"
-                  :on-success="(r, file) => onUploadSuccess(file, 'videoShortResource')" :action="userApi.video"
-                  auto-upload class="avatar-uploader" :multiple="false" :show-file-list="false">
-                  <video v-if="form.videoShortResource" style="width: 360px"
-                    :src="'http://123.57.13.5:82' + form.videoShortResource.url" controls></video>
-                  <el-icon v-else class="avatar-uploader-icon">
-                    <Plus />
-                  </el-icon>
-                </el-upload>
+                <div class="video">
+                  <el-upload name="image" :headers="authHeader"
+                    :on-success="(r, file) => onUploadSuccess(file, 'videoShortResource')" :action="userApi.video"
+                    auto-upload class="avatar-uploader" :multiple="false" :show-file-list="false">
+                    <video v-if="form.videoShortResource" style="width: 360px"
+                      :src="'http://123.57.13.5:82' + form.videoShortResource.url" controls></video>
+                    <el-icon v-else class="avatar-uploader-icon">
+                      <Plus />
+                    </el-icon>
+                  </el-upload>
+                  <el-tooltip v-if="form.videoShortResource" content="删除短视频" placement="top">
+                    <el-icon class="btn" @click="form.videoShortResource = null">
+                      <Delete />
+                    </el-icon>
+                  </el-tooltip>
+                </div>
+
               </div>
               <div>
                 <span> 商品头版图（横版） </span>
@@ -634,7 +642,7 @@
               <div>
                 <span class="desc"> *核保公司核保完成后,核保结果会自动在此呈现 </span>
                 <el-tooltip content="若有更多被保险人如拼团商品中非通过营探报名的出行人、主办方员工等需一并投保的，请在T-1日 (T为活动开始日期) 14时前在商品管理-保险投保模块上传其他被保险人的姓名
-                        证件号码、性别、出生年月，平台将在T-1日19时一次性向保险公司发送被保险人信息。因您自身原因导致上述被保险人信息未能报送成功的，营探不承担责任。" raw-content
+                              证件号码、性别、出生年月，平台将在T-1日19时一次性向保险公司发送被保险人信息。因您自身原因导致上述被保险人信息未能报送成功的，营探不承担责任。" raw-content
                   placement="top-start">
                   <el-icon>
                     <QuestionFilled />
@@ -701,16 +709,16 @@
               <div class="content">
                 <div>请填写说明会内容</div>
                 <el-input style="width:80%" type="textarea" autosize v-model="form.briefing.detail" placeholder="请出行人及监护人积极参加，说明会将针对下述内容展开：
-                        1、在线破冰活动
-                        2、在线安全培训
-                        3、答疑" />
+                              1、在线破冰活动
+                              2、在线安全培训
+                              3、答疑" />
               </div>
             </CampFormItem>
             <CampFormItem label="重要说明：" prop="mustKnow">
               <el-input style="width:80%" v-model="form.mustKnow" type="textarea" autosize placeholder="1.未成年人参加独立活动的,在开营当天需由监护人陪同办理相关手续，监护人需携带本人及未成年人身份证原件办理相关手续。 
-                        2.如因您隐瞒出行人情况导致不利后果，由您自行承担。
-                        3.出行人不应在活动过程中私自食用已知会导致您过敏的食物、酒类、不洁食品饮料，如您私自食用导致不良后果，本平台及服务商不承担任何责任。
-                                              " maxlength="500" show-word-limit />
+                              2.如因您隐瞒出行人情况导致不利后果，由您自行承担。
+                              3.出行人不应在活动过程中私自食用已知会导致您过敏的食物、酒类、不洁食品饮料，如您私自食用导致不良后果，本平台及服务商不承担任何责任。
+                                                    " maxlength="500" show-word-limit />
             </CampFormItem>
             <CampFormItem label="活动地点" prop="activityLocation.startLocation">
               <div style="width: 100%; display: flex; margin-bottom: 20px">
@@ -765,7 +773,7 @@
             </CampFormItem>
             <CampFormItem label="整理物资准备" prop="preparation">
               <el-input style="width:80%" v-model="form.preparation" type="textarea" autosize placeholder="1.家长可以提前准备一封关怀、鼓励孩子的信。
-                        2.有特长的同学携带设备参加活动,须提前沟通。" maxlength="500" show-word-limit>
+                              2.有特长的同学携带设备参加活动,须提前沟通。" maxlength="500" show-word-limit>
               </el-input>
             </CampFormItem>
             <CampFormItem label="团队紧急联系人方式" prop="emergencyContact.name">
@@ -2659,4 +2667,14 @@ const disabledDate2 = (time) => {
 .isShowPublicPage-footer {
   display: flex;
   justify-content: center;
-}</style>
+}
+
+.video {
+  display: flex;
+
+  .btn {
+    margin: auto 1vw;
+    cursor: pointer;
+  }
+}
+</style>
